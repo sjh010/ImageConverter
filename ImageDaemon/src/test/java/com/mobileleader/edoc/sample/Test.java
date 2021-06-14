ee40489ff8346ad533fa14935acb9221487f1e45
sjh010 <sjh010@so>
2021-06-14 10:27:18 +0900

    .

---
+++ ImageDaemon/src/test/java/com/mobileleader/edoc/sample/Test.java
@@ -0,0 +1,13 @@
+package com.mobileleader.edoc.sample;
+
+import org.apache.commons.io.FilenameUtils;
+
+public class Test {
+
+	public static void main(String[] args) {
+	
+		String name = "convert////convert.pdf";
+		
+		System.out.println(FilenameUtils.getName(name));
+	}
+}
