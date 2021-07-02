abfa3bf16ff5e013f26363ac471235b2f451cf7f
jhso <jhso@DESKTOP-2215KEU>
2021-07-02 14:20:46 +0900

    데몬 스케쥴링 삭제

--- ImageDaemon/src/main/java/com/mobileleader/image/deamon/ConvertDaemon.java
+++ ImageDaemon/src/main/java/com/mobileleader/image/deamon/ConvertDaemon.java
@@ -10,7 +10,6 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
@@ -22,8 +21,8 @@ import com.mobileleader.image.model.ConvertRequest;
 import com.mobileleader.image.model.ConvertResponse;
 import com.mobileleader.image.service.ImageConvertService;
 import com.mobileleader.image.socket.client.ImageConvertOutboundClient;
-import com.mobileleader.image.type.ResponseCodeType;
 import com.mobileleader.image.type.JobType;
+import com.mobileleader.image.type.ResponseCodeType;
 import com.mobileleader.image.util.JsonUtils;
 import com.mobileleader.image.util.TimeLogUtils;
 
@@ -40,9 +39,6 @@ public class ConvertDaemon {
 	@Autowired
 	@Qualifier("convertExecutor")
 	private ThreadPoolTaskExecutor convertExecutor;						// thread pool 관리를 위한 executor
-	
-	@Autowired
-	private ImageConvertOutboundClient imageConvertOutboundClient;		// 배치 요청 응답을 위한 client
 
 	@Autowired
 	private ImageConvertService imageConvertService;					// 이미지 변환 담당 서비스
@@ -50,10 +46,22 @@ public class ConvertDaemon {
 	@Autowired
 	private ConvertStatusMapper convertStatusMapper;					// 배치 변환 요청 상태 매퍼
 	
+	@Autowired
+	private ImageConvertOutboundClient imageConvertOutboundClient;		// 배치 요청 응답을 위한 client
+	
 	private PriorityQueue<ConvertRequest> requestQueue;					// 이미지 변환 요청 큐(우선순위 큐)
 	
-	private Map<String, ChannelFuture> channelFutureMap;				// 이미지 요청 ID별 응답을 위한 ChannelFuture Map
+	private Map<String, ChannelFuture> channelFutureMap;				// 이미지 요청 JOB ID별 응답을 위한 ChannelFuture Map
 	
+	public ConvertDaemon() {
+		this.requestQueue = new PriorityQueue<ConvertRequest>();
+		this.channelFutureMap = new HashMap<String, ChannelFuture>();
+	}
+	
+	/**
+	 * 이미지 변환 중에 데몬이 비정상 종료 되었을 경우, 
+	 * 데몬 재기동 시에 비정상 종료 전 이미지 변환 작업에 대한 실패 응답 전송  
+	 */
 	@PostConstruct
 	public void sendConvertFailResponse() {
 		List<ConvertStatus> statusList = convertStatusMapper.selectConvertFailList();
@@ -69,12 +77,21 @@ public class ConvertDaemon {
 		}
 	}
 	
-	public ConvertDaemon() {
-		this.requestQueue = new PriorityQueue<ConvertRequest>();
-		this.channelFutureMap = new HashMap<String, ChannelFuture>();
+	/**
+	 * 이미지 변환 작업에 사용할 Thread 개수 변경
+	 */
+	public void changeCoreThreadCount(int threadChangeCount) {
+		log.info("Change Thread Count : {} -> {}", convertExecutor.getCorePoolSize(), threadChangeCount);
+		convertExecutor.setMaxPoolSize(threadChangeCount);
+		convertExecutor.setCorePoolSize(threadChangeCount);
+		log.info("Thread pool size : {}", convertExecutor.getCorePoolSize());
+		
 	}
 
-	public void pushRequest(ConvertRequest request, ChannelFuture channelFuture) {
+	/**
+	 * 이미지 변환 요청을 이미지 변환 요청 큐에 삽입 후, 변환 작업 수행
+	 */
+	public void addRequest(ConvertRequest request, ChannelFuture channelFuture) {
 		log.info("[{}] add to Queue", request.getJobId());
 		
 		this.requestQueue.add(request);
@@ -90,19 +107,15 @@ public class ConvertDaemon {
 			convertStatusMapper.insertBatchConvertRequest(convertStatus);
 			TimeLogUtils.logging(startTime, System.currentTimeMillis(), "[" + request.getJobId() +"] DB insert");
 		}
+		
+		process();
 	}
 	
-	public void changeCoreThreadCount(int threadChangeCount) {
-		log.info("Change Thread Count : {} -> {}", convertExecutor.getCorePoolSize(), threadChangeCount);
-		convertExecutor.setCorePoolSize(threadChangeCount);
-		log.info("Thread core pool size : {}", convertExecutor.getCorePoolSize());
-		
-	}
-	
-	@Scheduled(cron = "* * * * * *")
-	public void processs() {
-		
-		// 큐에 요청이 쌓여 있는지 감시
+	/**
+	 * 이미지 변환 수행
+	 */
+	private void process() {
+		// 이미지 변환 요청에 Queue에 쌓여 있는지 확인
 		if (!requestQueue.isEmpty()) {
 			convertExecutor.execute(new Runnable() {
 				@Override
@@ -115,15 +128,19 @@ public class ConvertDaemon {
 					ChannelFuture channelFuture = channelFutureMap.get(request.getJobId());
 					
 					long startTime = System.currentTimeMillis();
+					
 					ConvertResponse response = imageConvertService.convert(request); // 이미지 변환
-					TimeLogUtils.logging(startTime, System.currentTimeMillis(), "[" + request.getJobId() + "] Convert image");
 					response.setDesRootPath(request.getDesRootPath());
+					
+					TimeLogUtils.logging(startTime, System.currentTimeMillis(), "[" + request.getJobId() + "] Convert image");
 					
 					if (JobType.REALTIME.getCode().equalsIgnoreCase(request.getJobType())) {		// 실시간 요청
 						try {
 							startTime = System.currentTimeMillis();
+							
 							channelFuture.channel().writeAndFlush(Unpooled.copiedBuffer(new Gson().toJson(response), CharsetUtil.UTF_8));
 							channelFuture.addListener(ChannelFutureListener.CLOSE);
+							
 							TimeLogUtils.logging(startTime, System.currentTimeMillis(), "[" + request.getJobId() + "] Send response (R)");
 						} catch (Exception e) {
 							log.error("Error", e);
@@ -132,10 +149,14 @@ public class ConvertDaemon {
 					} else if (JobType.BATCH.getCode().equalsIgnoreCase(request.getJobType())) {	// 배치 요청
 						try {
 							startTime = System.currentTimeMillis();
+							
 							imageConvertOutboundClient.sendResponse(response);
+							
 							TimeLogUtils.logging(startTime, System.currentTimeMillis(), "[" + request.getJobId() + "] Send response (B)");
 							startTime = System.currentTimeMillis();
+							
 							convertStatusMapper.deleteBatchConvertStatus(request.getJobId());
+							
 							TimeLogUtils.logging(startTime, System.currentTimeMillis(), "[" + request.getJobId() + "] DB delete");
 						} catch (Exception e) {
 							e.printStackTrace();
